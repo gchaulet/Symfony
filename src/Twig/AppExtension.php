8f902fc55b94a435f1f67fad2944cc49b9881615
Guillaume CHAULET <gchaulet@adws.amiltone.com>
2019-05-10 16:10:08 +0200

    variables globals extension

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,15 +2,36 @@
 
 namespace App\Twig;
 
+use Twig\TwigFilter;
 use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
 
-class AppExtension extends AbstractExtension
+class AppExtension extends AbstractExtension implements GlobalsInterface
 {
+    /**
+     * @var string
+     */
+    private $locale;
+
+    public function __construct(string $locale)
+    {
+        $this->locale = $locale;
+    }
+
     public function getFilters()
     {
         return [
             new TwigFilter('price', [$this, 'priceFilter'])
         ];
+    }
+
+    public function getGlobals()
+    {
+        return [
+            'locale' => $this->locale
+        ];
+        //on peut mettre {{ locale }} n'importe où dans le code  
+        
     }
 
     public function priceFilter($number)
